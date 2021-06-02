bfb50b67825fd1aec6a1eb26b255304f7c390218
Pedro Bini <dev.pedrobini@gmail.com>
2021-06-01 23:34:13 -0300

    chore: first commit

---
+++ lib/ecto_xml.ex
@@ -0,0 +1,18 @@
+defmodule EctoXml do
+  @moduledoc """
+  Documentation for `EctoXml`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> EctoXml.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ecto_xml_test.exs
@@ -0,0 +1,8 @@
+defmodule EctoXmlTest do
+  use ExUnit.Case
+  doctest EctoXml
+
+  test "greets the world" do
+    assert EctoXml.hello() == :world
+  end
+end
