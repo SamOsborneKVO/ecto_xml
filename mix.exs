2d3f2c07dc03d658e0caee2f69a9850c5b0a90f3
Pedro Bini <dev.pedrobini@gmail.com>
2021-06-02 18:02:08 -0300

    chore: do not consolidate protocols in test

--- mix.exs
+++ mix.exs
@@ -7,6 +7,7 @@ defmodule EctoXml.MixProject do
       version: "0.1.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
+      consolidate_protocols: Mix.env() != :test,
       test_coverage: [tool: ExCoveralls],
       preferred_cli_env: [
         coveralls: :test,
